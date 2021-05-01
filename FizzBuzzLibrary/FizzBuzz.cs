da4f0d790eca258d159eca38cceb6f7da189a830
brent00mi <brentrcrystal@gmail.com>
2021-04-30 21:40:54 -0400

    Add Class Library
    Add FizzBuzz class library

---
+++ FizzBuzzLibrary/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzLibrary
+{
+    public static class FizzBuzz
+    {
+        public static List<string> GetFizzBuzz()
+        {
+            List<string> output = new List<string>();
+
+            for (int i = 1; i <=100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    output.Add($" FizzBuzz ({i})");
+                }
+                else if (i % 3 == 0)
+                {
+                    output.Add($"Fizz ({i})");
+                }
+                else if (i % 5 == 0)
+                {
+                    output.Add($"Buzz ({i})");
+                }
+                else
+                {
+                    output.Add(i.ToString());
+                }
+            }
+
+            return output;
+        }
+
+        public static void PrintFizzBuzz(this List<string> fizzBuzz)
+        {
+            foreach (var item in fizzBuzz)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
